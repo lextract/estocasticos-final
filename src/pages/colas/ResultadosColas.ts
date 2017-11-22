e4c17658142fa4b4a8823047d3c83b477f7ff401
John Alex Antonio <jaantonior@unal.edu.co>
2017-11-21 23:27:20 -0500

    implementación de colas GGC

---
+++ src/pages/colas/ResultadosColas.ts
@@ -0,0 +1,8 @@
+export class ResultadosColas{
+    W: number;
+    L: number;
+    Wq: number;
+    Lq: number;
+    Ws: number;
+    Ls: number;
+}
---
+++ src/pages/colas/colaMMC.ts
@@ -0,0 +1,13 @@
+import { ResultadosColas } from './ResultadosColas';
+
+export default function calcularMMC(
+    lambda:number, 
+    miu: number,
+    servidores: number
+): ResultadosColas {
+    let resultados =  new ResultadosColas();
+
+    // TODO: implementar !!!
+
+    return resultados;
+}
